52fcea412e71dc77ebff51dba0175b4d4964c850
kaarla <sgakarla@ciencias.unam.mx>
2020-09-16 01:20:15 -0500

    bases para programota, antes de shutting

--- main.go
+++ main.go
@@ -3,30 +3,30 @@ package main
 import(
   "fmt"
   "encoding/json"
-//  "go/ast"
-//	"go/parser"
-//	"go/token"
+  "go/ast"
+	"go/parser"
+	"go/token"
 )
 
 func main(){
-    logicExpression1 := `{
-	                        "expression": "(amount > min)",
-	                        "save": "validAmount",
-                          "transitions": {
-		                           "isTrue": 5,
-		                           "isFalse": 10,
-		                           "isError": 25
-	                        },
-                          "context": {
-		                          "amount": 150,
-		                          "min": 45
-	                        }
-                        }`
-    var logicE LogicEx
-
-    json.Unmarshal([]byte(logicExpression1), &logicE)
-    //fmt.Printf("Expression: %s, Save: %s", logicE.Expression, logicE.Save)
-    fmt.Println(logicE)
+    // logicExpression1 := `{
+	  //                       "expression": "(amount > min)",
+	  //                       "save": "validAmount",
+    //                       "transitions": {
+		//                            "isTrue": 5,
+		//                            "isFalse": 10,
+		//                            "isError": 25
+	  //                       },
+    //                       "context": {
+		//                           "amount": 150,
+		//                           "min": 45
+	  //                       }
+    //                     }`
+    // var logicE LogicEx
+    //
+    // json.Unmarshal([]byte(logicExpression1), &logicE)
+    // //fmt.Printf("Expression: %s, Save: %s", logicE.Expression, logicE.Save)
+    // fmt.Println(logicE)
 
     arithmeticExpression1 := `{
 	                             "expression": "value/(99**2)",
